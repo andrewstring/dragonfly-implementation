73fd6378a3ead09ad5e5069135fbfdb29b4114c2
andrewstring <abreezemail@gmail.com>
2021-02-01 13:06:56 -0500

    added copy and paste shortcuts

--- grammar/rule/shortcut.py
+++ grammar/rule/shortcut.py
@@ -4,8 +4,10 @@ class ShortcutRule(MappingRule):
 
     mapping = {
         'cancel': Key('c-c'),
+        'copy': Key('c-c'),
         'desktop': Key('w-d'),
         'hide': Key('w-h'),
         'linux': Key('win'),
+        'paste': Key('c-v'),
         'save': Key('c-s')
     }
